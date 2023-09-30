8d42a8d25652bc55dedddedb176598a6acb8c470
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-09-30 15:12:17 +0300

    refactored and finished lab2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,83 +1,90 @@
-#!/bin/env python3
-import cv2 as cv
+#!/usr/bin/env python3
+import cv2
 import numpy as np
 
 
 def main():
-    # Load the colored image
-    im = cv.imread("../kepek/eper.jpg")
+    # Load the input color image (eper.jpg)
+    im = cv2.imread("../kepek/eper.jpg")
 
-    # Initialize the index for placing images in the mosaic
-    index = 0
+    # Task 1: Create images with one color channel set to zero
+    b, g, r = cv2.split(im)
+    zb = np.zeros_like(b)
+    zg = np.zeros_like(g)
+    zr = np.zeros_like(r)
 
-    # Create a larger canvas (mozaik) for displaying images
-    imBig = np.zeros((im.shape[0] * 6, im.shape[1] * 3, im.shape[2]), dtype=np.uint8)
-    imBig[:, :] = (128, 128, 255)  # Set the background color to light blue
+    im_b = cv2.merge([zb, g, r])
+    im_g = cv2.merge([b, zg, r])
+    im_r = cv2.merge([b, g, zr])
 
-    # Create an all-zero matrix of the same size as the input image
-    z = np.zeros_like(im)
-
-    # Task 1: Separate color channels (R, G, B)
-    b, g, r = cv.split(im)
-    showMyImage(imBig, cv.merge((b, z, z)), index)
-    showMyImage(imBig, cv.merge((z, g, z)), index)
-    showMyImage(imBig, cv.merge((z, z, r)), index)
-
-    # Task 2: Replace two color channels with zeros
-    showMyImage(imBig, cv.merge((b, g, z)), index)
-    showMyImage(imBig, cv.merge((b, z, r)), index)
-    showMyImage(imBig, cv.merge((z, g, r)), index)
+    # Task 2: Create images with two color channels set to zero
+    im_bg = cv2.merge([zb, zg, r])
+    im_gr = cv2.merge([b, zg, zr])
+    im_br = cv2.merge([zb, g, zr])
 
     # Task 3: Permute color channels
-    showMyImage(imBig, cv.merge((r, g, b)), index)
-    showMyImage(imBig, cv.merge((g, b, r)), index)
-    showMyImage(imBig, cv.merge((b, r, g)), index)
+    im_permuted_1 = cv2.merge([r, g, b])
+    im_permuted_2 = cv2.merge([r, b, g])
+    im_permuted_3 = cv2.merge([g, r, b])
+    im_permuted_4 = cv2.merge([g, b, r])
+    im_permuted_5 = cv2.merge([b, r, g])
+    im_permuted_6 = cv2.merge([b, g, r])
 
-    # Task 4: Replace one color channel with its negative
-    ib = cv.bitwise_not(b)
-    ig = cv.bitwise_not(g)
-    ir = cv.bitwise_not(r)
-    showMyImage(imBig, cv.merge((ib, g, r)), index)
-    showMyImage(imBig, cv.merge((b, ig, r)), index)
-    showMyImage(imBig, cv.merge((b, g, ir)), index)
+    # Task 4: Create images with one color channel replaced by its negative
+    im_neg_b = cv2.merge([~b, g, r])
+    im_neg_g = cv2.merge([b, ~g, r])
+    im_neg_r = cv2.merge([b, g, ~r])
 
-    # Task 5: Replace Y channel with its negative in YCrCb color space
-    ycrcb = cv.cvtColor(im, cv.COLOR_BGR2YCrCb)
-    y, cr, cb = cv.split(ycrcb)
-    ny = cv.bitwise_not(y)
-    showMyImage(
-        imBig,
-        cv.cvtColor(cv.merge((ny, cr, cb), flags=cv.COLOR_YCrCb2BGR), cv.COLOR_BGR2RGB),
-        index,
-    )
+    # Task 5: Replace the Y channel with its negative using YCrCb encoding
+    im_ycrcb = cv2.cvtColor(im, cv2.COLOR_BGR2YCrCb)
+    y, cr, cb = cv2.split(im_ycrcb)
+    im_neg_y = cv2.merge([~y, cr, cb])
+    im_neg_ycrcb = cv2.cvtColor(im_neg_y, cv2.COLOR_YCrCb2BGR)
 
-    # Task 6: Add the negative of the original image to the mosaic
-    negative_im = cv.bitwise_not(im)
-    showMyImage(imBig, negative_im, index)
+    # Task 6: Create the negative of the original image
+    im_neg = ~im
 
-    cv.destroyAllWindows()
+    # Create a big image to display results
+    im_big = np.zeros((im.shape[0] * 3, im.shape[1] * 6, im.shape[2]), dtype=np.uint8)
+    im_big[:] = (128, 128, 255)  # Set the background color
 
+    # Function to display images on the big image
+    def show_my_image(im_big, im, index):
+        im_copy = im.copy()
+        row_start = (index // 6) * im.shape[0]
+        row_end = row_start + im.shape[0]
+        col_start = (index % 6) * im.shape[1]
+        col_end = col_start + im.shape[1]
+        im_big[row_start:row_end, col_start:col_end] = im_copy
+        return index + 1
 
-def showMyImage(imBig, im, index):
-    row = index // 6
-    col = index % 6
+    index = 0
 
-    # Resize the input image to match the dimensions of imBig
-    im_resized = cv.resize(im, (imBig.shape[1] // 3, imBig.shape[0] // 6))
+    # Display images on the big image
+    index = show_my_image(im_big, im, index)  # Display the original RGB image
+    index = show_my_image(im_big, im_b, index)
+    index = show_my_image(im_big, im_g, index)
+    index = show_my_image(im_big, im_r, index)
+    index = show_my_image(im_big, im_gr, index)
+    index = show_my_image(im_big, im_br, index)
 
-    # Ensure that the resized image has 3 channels (BGR)
-    if im_resized.shape[2] != 3:
-        im_resized = cv.cvtColor(
-            im_resized, cv.COLOR_GRAY2BGR
-        )  # Convert to BGR if not already
+    index = show_my_image(im_big, im_bg, index)
+    index = show_my_image(im_big, im_permuted_6, index)
+    index = show_my_image(im_big, im_permuted_5, index)
+    index = show_my_image(im_big, im_permuted_4, index)
+    index = show_my_image(im_big, im_permuted_3, index)
+    index = show_my_image(im_big, im_permuted_2, index)
 
-    imBig[
-        row * im_resized.shape[0] : (row + 1) * im_resized.shape[0],
-        col * im_resized.shape[1] : (col + 1) * im_resized.shape[1],
-    ] = im_resized
+    index = show_my_image(im_big, im_permuted_1, index)
+    index = show_my_image(im_big, im_neg_b, index)
+    index = show_my_image(im_big, im_neg_g, index)
+    index = show_my_image(im_big, im_neg_r, index)
+    index = show_my_image(im_big, im_neg_ycrcb, index)
+    show_my_image(im_big, im_neg, index)
 
-    cv.imshow("Ablak", imBig)
-    cv.waitKey(0)
+    cv2.imshow("Ablak", im_big)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
