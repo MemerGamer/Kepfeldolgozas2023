0976b144ee79468b7636cca749986911266e2da9
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-10-12 15:25:41 +0300

    finished lab4

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -8,9 +8,44 @@ Mvn = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]])
 Mfp = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
 Mfn = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
 
+# Define parameters for canny filter
+max_low_threshold = 100
+title_trackbar = "Min Threshold:"
+ratio = 3
+kernel_size = 3
+
 
 def main():
-    image = cv2.imread("../kepek/bogyok.jpg", cv2.IMREAD_GRAYSCALE)
+    print("Lab 4: Image filtering")
+    img_path = "../kepek/bogyok.jpg"
+    while True:
+        print("Choose a task:")
+        print("1. Gradient filters")
+        print("2. Canny filter")
+        print("q. Quit")
+
+        choice = input()
+        if choice == "1":
+            gradient_filters(img_path)
+        elif choice == "2":
+            canny_filters(img_path)
+        elif choice == "q":
+            break
+        else:
+            print("Invalid choice. Please select a valid task.")
+    print("Bye!")
+
+
+def load_image(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        print("Error: Image not found")
+        exit(1)
+    return image
+
+
+def gradient_filters(image_path):
+    image = load_image(image_path)
 
     # Apply the masks
     grad_vp = cv2.filter2D(image, -1, Mvp)
@@ -39,6 +74,38 @@ def main():
     cv2.imshow("All edges", all_edges)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def on_threshold_change(image, val, windows_name):
+    low_threshold = val
+    img_blur = cv2.blur(image, (3, 3))
+    detected_edges = cv2.Canny(
+        img_blur, low_threshold, low_threshold * ratio, kernel_size
+    )
+    mask = detected_edges != 0
+    mask = mask.astype(np.uint8)
+    img_canny = image * mask
+    cv2.imshow(windows_name, img_canny)
+
+
+def canny_filters(image_path):
+    # Read the image
+    image = load_image(image_path)
+
+    # Create window for displaying canny image
+    windows_name = "Canny Filter"
+    cv2.namedWindow(windows_name)
+
+    # Create trackbar for canny threshold
+    cv2.createTrackbar(
+        title_trackbar,
+        windows_name,
+        0,
+        max_low_threshold,
+        lambda val: on_threshold_change(image, val, windows_name),
+    )
+    on_threshold_change(image, 0, windows_name)
+    cv2.waitKey(0)
 
 
 if __name__ == "__main__":
