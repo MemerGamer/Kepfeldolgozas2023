e3e1122a0a863772c39a291bbc6e8dacd048753b
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-10-26 16:13:44 +0300

    renamed variables according to class lab7

--- lab7/lab7_1.py
+++ lab7/lab7_1.py
@@ -5,38 +5,38 @@ import numpy as np
 
 def main():
     # Load the binary image of the amoeba
-    amoeba_image = cv2.imread("../kepek/amoba.png", cv2.IMREAD_GRAYSCALE)
+    imBe = cv2.imread("../kepek/amoba.png", cv2.IMREAD_GRAYSCALE)
 
     # Check if the image was loaded successfully
-    if amoeba_image is None:
+    if imBe is None:
         print("Error: Image not loaded.")
         return
 
     # Display the input image
-    cv2.imshow("Input Image", amoeba_image)
+    cv2.imshow("Input Image", imBe)
     cv2.waitKey(0)
 
     # Perform the distance transformation
-    distance_map = cv2.distanceTransform(amoeba_image, cv2.DIST_L2, 3)
+    imD = cv2.distanceTransform(imBe, cv2.DIST_L2, 3)
 
     # Convert the 32-bit output to 8-bit
-    distance_map = cv2.normalize(distance_map, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    imD = cv2.normalize(imD, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 
     # Display the distance map image
-    cv2.imshow("Dilated Image", distance_map)
+    cv2.imshow("Dilated Image", imD)
     cv2.waitKey(0)
 
     # Create a 5x5 circular structuring element
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
 
     # Dilate the distance map
-    dilated_distance_map = cv2.dilate(distance_map, kernel)
+    imX = cv2.dilate(imD, kernel)
 
     # TODO: Copy the dilated image back to the distance map where the original distance map is non-zero
-    result = np.where(distance_map > 0, dilated_distance_map, distance_map)
+    imKi = np.where(imD > 0, imX, imD)
 
     # Display the final image representing amoeba diameter
-    cv2.imshow("Final Image", result)
+    cv2.imshow("Final Image", imKi)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
